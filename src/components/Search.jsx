2b065bff994ebe7b7320344e8509d6469cc6c55d
harianchery <hari.anchery001@gmail.com>
2024-06-08 23:22:03 +0530

    Search file created with form value reading

---
+++ src/components/Search.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+    const [news,changeNews]=useState(
+        {
+            "Title":""
+        }
+    )
+    const InputHandler=(event)=>{
+        changeNews({...news,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(news)
+    }
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Title</label>
+                            <input type="text" className="form-control" name='Title' value={news.Title} onChange={InputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-primary" onClick={readValue}>Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Search
